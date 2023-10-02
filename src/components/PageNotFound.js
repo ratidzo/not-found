e0ecf2c5285e040ef507aab6a937b4177515cd33
ratidzo <ratygore@gmail.com>
2023-10-02 11:31:09 +0200

    add responsive breakpoint 'sm'

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -7,19 +7,21 @@ export default function PageNotFound() {
     
         >
             <div className="text-center">
-                <p className="text-2xl font-semibold text-gray-100">404</p>
+                <p className="sm:text-2xl font-semibold text-gray-100">404</p>
                 <h1 className="mt-4 text-3xl font-extrabold tracking-tight
                 text-gray-100 sm:text-6xl">Page not found</h1>
-                <p className="mt-6 text-lg leading-7 text-gray-300 ">Sorry, we couldn't find the page you're looking for.</p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
+                <p className="mt-6 sm:text-lg leading-7 text-gray-300 ">Sorry, we couldn't find the page you're looking for.</p>
+                <div className="mt-10 flex items-center justify-center
+                gap-x-2 sm:gap-x-6">
                     <a href="#"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-lg
-                    font-semibold text-white shadow-sm hover:bg-indigo-500
+                    className="rounded-md bg-teal-600 px-3.5 py-2.5 text-sm sm:text-lg
+                    font-semibold text-white shadow-sm hover:bg-teal-500
                     focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ">
                         <span>&larr;</span> Go back home
                     </a>
                     <a href="#" 
-                    className="text-lg font-semibold text-gray-100">
+                    className="text-sm sm:text-lg font-semibold text-gray-100
+                    rounded-md hover:bg-gray-100/20 hover:backdrop-blur px-3.5 py-2.5">
                         Contact support <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
