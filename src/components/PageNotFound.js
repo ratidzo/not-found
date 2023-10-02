362cc6f8640b527e1d50023307bc453e79fba3b0
ratidzo <ratygore@gmail.com>
2023-10-02 10:14:53 +0200

    add style to buttons

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -6,11 +6,15 @@ export default function PageNotFound() {
                 <h1>Page not found</h1>
                 <p>Sorry, we couldn't find the page you're looking for.</p>
                 <div>
-                    <a href="#">
+                    <a href="#"
+                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm
+                    font-semibold text-white shadow-sm hover:bg-indigo-500
+                    focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ">
                         Go back home
                     </a>
-                    <a href="#">
-                        Contact support <span>&rarr;</span>
+                    <a href="#" 
+                    className="text-sm font-semibold text-gray-900">
+                        Contact support <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
             </div>
