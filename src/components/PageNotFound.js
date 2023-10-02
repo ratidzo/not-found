d0ba04ddd71dbb0f2f5434dfbd193d2eba1b9ced
ratidzo <ratygore@gmail.com>
2023-10-02 10:18:35 +0200

    add style to button parent div

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -5,7 +5,7 @@ export default function PageNotFound() {
                 <p>404</p>
                 <h1>Page not found</h1>
                 <p>Sorry, we couldn't find the page you're looking for.</p>
-                <div>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
                     <a href="#"
                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm
                     font-semibold text-white shadow-sm hover:bg-indigo-500
