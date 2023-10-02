db6e16e49baec03899bfde26a1e7cb5990ab00b2
ratidzo <ratygore@gmail.com>
2023-10-02 11:12:32 +0200

    setup text color to match background

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -7,19 +7,19 @@ export default function PageNotFound() {
     
         >
             <div className="text-center">
-                <p className="text-base font-semibold text-indigo-400">404</p>
-                <h1 className="mt-4 text-3xl font-bold tracking-tight
-                text-gray-900 sm:text-5xl">Page not found</h1>
-                <p className="mt-6 text-base leading-7 text-gray-600 ">Sorry, we couldn't find the page you're looking for.</p>
+                <p className="text-2xl font-semibold text-gray-100">404</p>
+                <h1 className="mt-4 text-3xl font-extrabold tracking-tight
+                text-gray-100 sm:text-6xl">Page not found</h1>
+                <p className="mt-6 text-lg leading-7 text-gray-300 ">Sorry, we couldn't find the page you're looking for.</p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                     <a href="#"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm
+                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-lg
                     font-semibold text-white shadow-sm hover:bg-indigo-500
                     focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ">
-                        Go back home
+                        <span>&larr;</span> Go back home
                     </a>
                     <a href="#" 
-                    className="text-sm font-semibold text-gray-900">
+                    className="text-lg font-semibold text-gray-100">
                         Contact support <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
