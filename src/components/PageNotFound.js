8b26768138abe185084bfb7048e9f347f4d0808f
ratidzo <ratygore@gmail.com>
2023-10-02 10:24:20 +0200

    add style to error message header

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,7 +3,8 @@ export default function PageNotFound() {
         <main>
             <div>
                 <p>404</p>
-                <h1 className="">Page not found</h1>
+                <h1 className="mt-4 text-3xl font-bold tracking-tight
+                text-gray-900 sm:text-5xl">Page not found</h1>
                 <p className="mt-6 text-base leading-7 text-gray-600 ">Sorry, we couldn't find the page you're looking for.</p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                     <a href="#"
