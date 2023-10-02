c88a52d3496c76d9c0ebd64822358f44677ae752
ratidzo <ratygore@gmail.com>
2023-10-02 10:07:27 +0200

    create component `PageNotFound`
    Add unstyled page markup

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,19 @@
+export default function PageNotFound() {
+    return(
+        <main>
+            <div>
+                <p>404</p>
+                <h1>Page not found</h1>
+                <p>Sorry, we couldn't find the page you're looking for.</p>
+                <div>
+                    <a href="#">
+                        Go back home
+                    </a>
+                    <a href="#">
+                        Contact support <span>&rarr;</span>
+                    </a>
+                </div>
+            </div>
+        </main>
+    )
+}
