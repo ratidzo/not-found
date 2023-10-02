9561c93d4e4495f0058389099cc676ad7f0686be
ratidzo <ratygore@gmail.com>
2023-10-02 10:27:46 +0200

    add style to error code

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -2,7 +2,7 @@ export default function PageNotFound() {
     return(
         <main>
             <div>
-                <p>404</p>
+                <p className="text-base font-semibold text-indigo-400">404</p>
                 <h1 className="mt-4 text-3xl font-bold tracking-tight
                 text-gray-900 sm:text-5xl">Page not found</h1>
                 <p className="mt-6 text-base leading-7 text-gray-600 ">Sorry, we couldn't find the page you're looking for.</p>
